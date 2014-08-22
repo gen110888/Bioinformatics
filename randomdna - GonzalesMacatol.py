d3b417ac2e3ee9a0e5db73ca9dd4ffb17e1d5fb5
gen110888 <gen110888@hotmail.com>
2014-08-22 11:01:11 +0800

    Update randomdna - GonzalesMacatol.py

--- randomdna - GonzalesMacatol.py
+++ randomdna - GonzalesMacatol.py
@@ -1,9 +1,11 @@
 #MSIT/MIT 2nd Year 1st Semester
 #IT 440 Bioinformatics
-#Exercise 3.1
+#Exercise 3.1: randomdna
 #by: Genesis Gonzales and Khris Macatol
 
-#Hi Ma'am Frances Vega, this program runs best in Python 3.x.
+#Hi Ma'am Frances Vega, this program runs best in Python 2.x.
+#Also, the random DNA sequence output cuts to the next line every after 60th character
+#to be similar to your samples as Fasta format.
 
 #Division of labor:
 #Genesis Gonzales - created the random sequence syntax part of the program.
@@ -13,25 +15,43 @@
 #1. We created our individual assigned task.
 #2. We combined our work together to finish the overall task.
 
+#Start.
+
 import random
 
-length=int(input("Please enter the length of the random DNA sequence: "))
-print()
+b=True
+while(b):
+    length=0
 
-print(">myrandomsequence")
+    try:
+        length=int(input('Please input the length of the random DNA sequence then press "Enter": '))
+    except:
+        continue
+    print ""
+
+    print(">myrandomsequence")
 
 
-sequence=""
+    sequence=""
 
-for i in range(0,length):
-    sequence+=random.choice(["A","T","C","G"])
-    if(len(sequence)%60==0):
-        print(sequence)
-        sequence=""
+    for i in range(0,length):
+        
+        sequence+=random.choice(["A","T","C","G"])
+        if(len(sequence)%60==0):
+            print(sequence)
+            sequence=""
 
-print(sequence)
 
-print()
-input("Press enter to quit.")
+    print(sequence)
+
+    print""
+    while True:
+        inpt=raw_input('Please press "Enter" to input the length of the random DNA sequence again or input "Q" / "q" then press "Enter" to quit: ')
+
+        if(inpt=="Q" or inpt=="q" or inpt=="" or inpt=="\n"):
+            break
+
+    if(inpt=="Q" or inpt=="q"):
+        break
 
 #End.
